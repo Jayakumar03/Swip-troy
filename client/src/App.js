c03effdb70fc82bccbc1ed52b1782de04c9bac29
jayakumar03 <jaimech61@gmail.com>
2023-11-26 13:06:53 +0530

    Created new route for individual story

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import Home from "./Components/Home";
 import Bookmarkpage from "./Components/Bookmarkpage";
 
-import Indi from "./Components/individualstories/IndividualStory";
+import IndividualStory from "./Components/individualstories/IndividualStory";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/bookmark" element={<Bookmarkpage />} />
-      {/* <Route path="/" element={<Indi />} /> */}
+      <Route path="/bookmark/:id" element={<Bookmarkpage />} />
+      <Route path="/individualstory/:id" element={<IndividualStory />} />
     </Routes>
   );
 };
