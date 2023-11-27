54606fc03793d8e4262b4245cebdea2d23ecf2c2
jayakumar03 <jaimech61@gmail.com>
2023-11-27 12:34:57 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 // This will allow all CORS requests
 app.use(cors());
 
-// preflight request 
-app.options('*', cors());
+// preflight request
+app.options("*", cors());
 
 // ? Regular Middleware
 app.use(express.json());
@@ -18,22 +18,13 @@ const fileUpload = require("express-fileupload");
 require("dotenv").config();
 mongoDB();
 
-
-
 // Import all routes here
 const user = require("./routes/user");
-const stories = require("./routes/stories")
+const stories = require("./routes/stories");
 
 // Router Middleware
 app.use("/api/v1/", user);
-app.use("/api/v1/stories/", stories)
-
-
-
-
-
-
-
+app.use("/api/v1/stories/", stories);
 
 const port = process.env.PORT || 4000;
 app.listen(port, console.log("server is running at " + port, "..."));
@@ -44,7 +35,6 @@ app.get("/health", (req, res) => {
   console.log("Health is good");
 });
 
-
 app.get("/", (req, res) => {
-  res.status(200).send(<h1>Home</h1>)
+  res.status(200).send("<h1>Home</h1>");
 });
